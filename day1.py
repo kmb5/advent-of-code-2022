6760547e464cd44db54432663150b4cdb14f0d21
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-12-01 10:01:22 +0100

    day 1 solved

---
+++ day1.py
@@ -0,0 +1,47 @@
+TEST_STR = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+
+def prepare_lst(str: str) -> list[list[int]]:
+    """Convert str to a list of lists by creating new lists for every double newline"""
+    return [[int(i) for i in part.split("\n")] for part in str.split("\n\n")]
+
+
+def part1(lst: list) -> int:
+    """Return the max sum of the sublists in lst"""
+    return max(sum(el) for el in lst)
+
+
+def part2(lst: list) -> int:
+    """Return the total sum of the sum of the three largest sublists"""
+    return sum(sorted((sum(el) for el in lst), reverse=True)[:3])
+
+
+def main():
+
+    with open("inputs/day1.txt") as f:
+        str = f.read()[:-2]  # trim ending newline
+
+    prepared_list = prepare_lst(str)
+
+    part1_sol = part1(prepared_list)
+    print(f"Part 1: {part1_sol}")
+
+    part2_sol = part2(prepared_list)
+    print(f"Part 2: {part2_sol}")
+
+
+if __name__ == "__main__":
+    main()
